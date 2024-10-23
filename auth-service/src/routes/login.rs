fcbd6c5c9183de609c34fcba0c2785d86872a35f
Cezar Crintea <cezar.crintea@gmail.com>
2024-10-23 22:32:37 +0300

    [Sprint 3][Task 1] feat: Implement log-in route error cases

--- auth-service/src/routes/login.rs
+++ auth-service/src/routes/login.rs
@@ -1,5 +1,44 @@
-use axum::{http::StatusCode, response::IntoResponse};
+use axum::{extract::State, http::StatusCode, response::IntoResponse, Json};
+use serde::Deserialize;
 
-pub async fn login() -> impl IntoResponse {
-    StatusCode::OK.into_response()
+use crate::{
+    app_state::AppState,
+    domain::{AuthAPIError, Email, Password},
+};
+
+pub async fn login(
+    State(state): State<AppState>,
+    Json(request): Json<LoginRequest>,
+) -> Result<impl IntoResponse, AuthAPIError> {
+    let email = Email::parse(request.email);
+    if email.is_err() {
+        return Err(AuthAPIError::InvalidCredentials);
+    }
+    let email = email.unwrap();
+
+    let password = Password::parse(request.password);
+    if password.is_err() {
+        return Err(AuthAPIError::InvalidCredentials);
+    }
+    let password = password.unwrap();
+
+    let user_store = &state.user_store.read().await;
+
+    user_store
+        .validate_user(&email, &password)
+        .await
+        .map_err(|_| AuthAPIError::IncorrectCredentials)?;
+
+    let user = user_store
+        .get_user(&email)
+        .await
+        .map_err(|_| AuthAPIError::IncorrectCredentials)?;
+
+    Ok(StatusCode::OK.into_response())
+}
+
+#[derive(Deserialize)]
+pub struct LoginRequest {
+    pub email: String,
+    pub password: String,
 }
--- auth-service/tests/api/login.rs
+++ auth-service/tests/api/login.rs
@@ -1,10 +1,102 @@
-use crate::helpers::TestApp;
+use auth_service::ErrorResponse;
+
+use crate::helpers::{get_random_email, TestApp};
 
 #[tokio::test]
-async fn login_returns_200() {
+async fn should_return_422_if_malformed_credentials() {
     let app = TestApp::new().await;
 
-    let response = app.post_login().await;
+    let random_email = get_random_email();
 
-    assert_eq!(response.status().as_u16(), 200);
+    let test_cases = [
+        serde_json::json!({
+            "password": "password123",
+        }),
+        serde_json::json!({
+            "email":random_email,
+        }),
+        serde_json::json!({}),
+    ];
+
+    for test_case in test_cases.iter() {
+        let response = app.post_signup(test_case).await;
+        assert_eq!(
+            response.status().as_u16(),
+            422,
+            "Failed for input: {:?}",
+            test_case
+        );
+    }
+}
+
+#[tokio::test]
+async fn should_return_400_if_invalid_input() {
+    let app = TestApp::new().await;
+
+    let inputs = [
+        serde_json::json!({
+            "email":"",
+            "password": "password123",
+        }),
+        serde_json::json!({
+            "email":"test",
+            "password": "password123",
+        }),
+        serde_json::json!({
+            "email": "test@example.com",
+            "password": "pass123",
+        }),
+    ];
+
+    for input in inputs.iter() {
+        let response = app.post_login(input).await;
+
+        assert_eq!(
+            response.status().as_u16(),
+            400,
+            "Failed for input: {:?}",
+            input
+        );
+
+        assert_eq!(
+            response
+                .json::<ErrorResponse>()
+                .await
+                .expect("Could not deserialize response body to ErrorResponse")
+                .error,
+            "Invalid credentials".to_owned()
+        );
+    }
+}
+
+#[tokio::test]
+async fn should_return_401_if_incorrect_credentials() {
+    let app = TestApp::new().await;
+    let test_case = serde_json::json!({
+        "email":"test@example.com",
+        "password": "password123",
+        "requires2FA": true
+    });
+
+    let response = app.post_signup(&test_case).await;
+
+    assert_eq!(response.status().as_u16(), 201);
+
+    let test_case = serde_json::json!({
+        "email":"test@example.com",
+        "password": "password456"
+    });
+
+    let response = app.post_login(&test_case).await;
+
+    assert_eq!(response.status().as_u16(), 401);
+
+    assert_eq!(
+        response
+            .json::<ErrorResponse>()
+            .await
+            .expect("Could not deserialize response body to ErrorResponse")
+            .error,
+        "Incorrect credentials".to_owned()
+    );
 }
