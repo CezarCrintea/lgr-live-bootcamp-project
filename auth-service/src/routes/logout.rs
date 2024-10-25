90510c153df4850148e14c6dde8e68fd7840701e
Cezar Crintea <cezar.crintea@gmail.com>
2024-10-25 14:22:21 +0300

    [Sprint 3][Task 3] feat: Implement log-out route

--- auth-service/src/routes/logout.rs
+++ auth-service/src/routes/logout.rs
@@ -1,5 +1,22 @@
 use axum::{http::StatusCode, response::IntoResponse};
+use axum_extra::extract::CookieJar;
 
-pub async fn logout() -> impl IntoResponse {
-    StatusCode::OK.into_response()
+use crate::{domain::AuthAPIError, utils::auth::validate_token};
+
+pub async fn logout(jar: CookieJar) -> (CookieJar, Result<impl IntoResponse, AuthAPIError>) {
+    let cookie = match jar.get("jwt") {
+        Some(cookie) => cookie,
+        None => return (jar, Err(AuthAPIError::MissingToken)),
+    };
+
+    let token = cookie.value().to_owned();
+
+    match validate_token(&token).await {
+        Ok(_) => (),
+        Err(_) => return (jar, Err(AuthAPIError::InvalidToken)),
+    }
+
+    let jar = jar.remove("jwt");
+
+    (jar, Ok(StatusCode::OK))
 }
